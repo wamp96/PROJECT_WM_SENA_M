ead20e1a5051afb7c2b5ee47c025369fc135aad6
wamp96 <wamoreno57@gmail.com>
2024-09-14 10:15:59 -0500

    commit uploading and deploying api to git

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      usuario: [],
+      url: 'http://192.168.1.19:8080/api/users' // Cambia esto a la IP del servidor en red si usas un dispositivo físico
+    };
+  }
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  getUser = () => {
+    this.setState({ loading: true });
+    fetch(this.state.url)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          usuario: response.users,
+          loading: false
+        });
+      })
+      .catch(error => {
+        console.error(error);
+        this.setState({ loading: false });
+      });
+  };
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <View style={styles.container}>
+          <Text>Loading...</Text>
+        </View>
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.usuario}
+          keyExtractor={item => item.User_id.toString()} // Asegúrate de que 'id' existe en los datos de usuario
+          renderItem={({ item }) => <Text>{item.User_nombre}</Text>}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
